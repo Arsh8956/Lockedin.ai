2736ef1649a836d86905833cf3a0d5e59e759f27
Arsh Guirand <arshguirand@gmail.com>
2025-03-12 01:17:06 -0400

    server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 print(f"Starting server at http://localhost:{PORT}")
-print("Press Ctrl+C to stop the server")
+print("Press Ctrl+C to stop the server") 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     httpd.serve_forever() 
